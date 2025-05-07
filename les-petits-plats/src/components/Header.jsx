b2a1a47b7ecce716cf759fa64e5e0ffdc34fbebb
Amin <amin.eddine31@icloud.com>
2025-05-07 16:07:38 +0200

    header

--- les-petits-plats/src/components/Header.jsx
+++ les-petits-plats/src/components/Header.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import '../styles/Header.css';
+import logo from '../images/Logo.png';
 
-function App() {
-
+const Header = () => {
   return (
-    <>
-    </>
-  )
-}
+    <header className="header">
+     <div className="header-top">
+        <img src={logo} alt="Logo Les Petits Plats" className="full-logo" />
+      </div>
 
-export default App
+      <div className="overlay">
+        <h1 className="title">
+          CHERCHEZ PARMI PLUS DE 1500 RECETTES<br />
+          DU QUOTIDIEN, SIMPLES ET DÉLICIEUSES
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
